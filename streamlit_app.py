6af012f924bf45b3701d4ff56aa347284c181d14
sphanse2021 <87150932+sphanse2021@users.noreply.github.com>
2024-09-26 11:21:35 -0400

    removed import from snowflake,snowpark active_session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import pandas as pd
+
+
 
 st.title("Zena's Amazing Athleisure Catalog")
 
